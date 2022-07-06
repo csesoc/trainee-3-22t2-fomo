b50db2478a95bf93b82ad8eb50b2f46130063aca
Theo Ang <theo.ang816@gmail.com>
2022-07-06 11:33:50 +1000

    frontend for loginpage (rough no functionality)

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import EventAdd from './components/EventAdd';
+import LoginPage from './components/LoginPage';
 
 function App() {
   return (
     <div className="App">
-      <EventAdd />
+      <LoginPage />
     </div>
   );
 }
