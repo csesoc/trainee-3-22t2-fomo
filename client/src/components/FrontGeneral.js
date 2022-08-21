32f923b30d6176590abada2607dd37410b64d37d
jasttran <jasminetran2003@gmail.com>
2022-08-21 19:59:03 +1000

    added text to info button

--- client/src/components/FrontGeneral.js
+++ client/src/components/FrontGeneral.js
@@ -7,6 +7,8 @@ import Profile from './LoginRegister/Profile';
 import './FrontGeneral.css'
 import { useState } from 'react'; 
 
+import InfoButton from './InfoButton';
+
 const FrontGeneral = () => {
   const [buttonPopup, setButtonPopup] = useState(false);
   return (
@@ -15,7 +17,13 @@ const FrontGeneral = () => {
         <img className='logo' src={mainlogo} /> 
         <ul>
           <li> <EventAdd /> </li>
-          <li> <FaInfo /> </li>
+          <li> <FaInfo onClick={()=>setButtonPopup(true)}/> </li>
+
+          <InfoButton trigger={buttonPopup} setTrigger={setButtonPopup}>
+            <h1>FOMO</h1>
+            <p>♡ Never let your bad organisation skills make you feel fomo again ♡</p>
+          </InfoButton>
+          
           <li> <FaBars /> </li>
           <li> <LoginIcon /> </li>
         </ul>
