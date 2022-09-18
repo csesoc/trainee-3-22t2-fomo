0d43f0d0a840ef2aa0572af802e5e58a63b64d3c
jasttran <jasminetran2003@gmail.com>
2022-09-18 12:54:47 +1000

    styled info page

--- client/src/components/NavBar/InfoButton.js
+++ client/src/components/NavBar/InfoButton.js
@@ -18,13 +18,16 @@ const InfoButton = () => {
     <div>
       <FaInfo className="close-btn" onClick={handleOpen} /> 
       <Dialog open={open} onClose={handleClose}> 
-
+      
       <div className='info-block'>
+      <DialogActions>
+          <FontAwesomeIcon icon={faTimes} onClick={handleClose}>Cancel</FontAwesomeIcon> 
+        </DialogActions>
         <Box sx={{ display: 'flex', flexWrap: 'wrap', padding: '30px' }}>  
+        <div className='info-info'>  
             <h1>FOMO</h1> 
-            <br />
             <h4>♡ Never let your bad organisation skills make you feel fomo again ♡</h4>   
-            <br />
+            < br/>
             <h3>How to Use</h3>
                   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Turpis in eu mi bibendum neque egestas congue quisque egestas. Ipsum faucibus vitae aliquet nec ullamcorper sit amet risus. Malesuada pellentesque elit eget gravida cum sociis natoque penatibus et. Elit sed vulputate mi sit amet. Leo duis ut diam quam nulla porttitor massa id. Sit amet est placerat in egestas. Vitae congue eu consequat ac felis donec. Elit scelerisque mauris pellentesque pulvinar. Fermentum iaculis eu non diam phasellus. Dis parturient montes nascetur ridiculus mus mauris. Integer eget aliquet nibh praesent tristique magna. Cursus euismod quis viverra nibh. Elementum nisi quis eleifend quam adipiscing.</p>
                   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Turpis in eu mi bibendum neque egestas congue quisque egestas. Ipsum faucibus vitae aliquet nec ullamcorper sit amet risus. Malesuada pellentesque elit eget gravida cum sociis natoque penatibus et. Elit sed vulputate mi sit amet. Leo duis ut diam quam nulla porttitor massa id. Sit amet est placerat in egestas. Vitae congue eu consequat ac felis donec. Elit scelerisque mauris pellentesque pulvinar. Fermentum iaculis eu non diam phasellus. Dis parturient montes nascetur ridiculus mus mauris. Integer eget aliquet nibh praesent tristique magna. Cursus euismod quis viverra nibh. Elementum nisi quis eleifend quam adipiscing.</p>
@@ -39,11 +42,10 @@ const InfoButton = () => {
                     <li>Nathan</li>
                     <li>Sofia</li>
                     <li>Jasmine</li>
-                  </ul>  
+                  </ul> 
+          </div>
         </Box>
-        <DialogActions>
-          <FontAwesomeIcon icon={faTimes} onClick={handleClose}>Cancel</FontAwesomeIcon> 
-        </DialogActions>
+         
       </div>
     </Dialog>
     </div>
