00372e873928c9a46c6eae11291e9d8ffbc32f23
jasttran <jasminetran2003@gmail.com>
2022-09-20 18:51:35 +1000

    removed redundant code

--- client/src/components/NavBar/FrontGeneral.js
+++ client/src/components/NavBar/FrontGeneral.js
@@ -1,19 +1,14 @@
 import mainlogo from '../../mainlogo.png' 
-
-import { FaInfo, FaClock } from "react-icons/fa"
 import EventAdd from './EventAdd';
 import LoginIcon from '../LoginRegister/LoginIcon';
 import Profile from '../LoginRegister/Profile';
 import './FrontGeneral.css'
-import { useState } from 'react'; 
 import InfoButton from './InfoButton'; 
 import Calendar from '../Calendar'; 
 import OppositeContentTimeline from './Timeline';
 
 
 const FrontGeneral = () => {
-  const [buttonPopup, setButtonPopup] = useState(false);
-  const [buttonPopup2, setButtonPopup2] = useState(false);
   return (
     <div>
       <div className='navBar'>
