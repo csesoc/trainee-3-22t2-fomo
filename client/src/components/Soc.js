dd9c227e16b7f7d7d47674d0282f0cd4d2f3c112
Theo Ang <theo.ang816@gmail.com>
2022-09-28 15:11:26 +1000

    societies remove x button work like tags

--- client/src/components/Soc.js
+++ client/src/components/Soc.js
@@ -1,17 +1,18 @@
-import { FaTimes } from 'react-icons/fa'
 import styles from './Soc.module.css'
 
 const Soc = ({ society, addSociety, delSociety }) => {
 
   const onClick = (e) => {
-    addSociety(society._id);
-    // e.currentTarget.classList.toggle(`${styles.followSocActive}`)
+    if (e.currentTarget.classList.length > 1) {
+      delSociety(society._id);
+    } else {
+      addSociety(society._id);
+    }
   }
     
   return (
     <div className={styles.followSoc} onClick={onClick}>
         <p>{society.societyName}</p>
-        <FaTimes onClick={() => delSociety(society._id)} size={20}/>
     </div>
   )
 }
