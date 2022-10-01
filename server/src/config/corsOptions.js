1d098580a5adcba3fd67b4f073292beb84f9d261
henryg <henryguo78@gmail.com>
2022-10-01 19:59:16 +1000

    Added debugging for cors

--- server/src/config/corsOptions.js
+++ server/src/config/corsOptions.js
@@ -2,6 +2,7 @@ import { allowedOrigins } from './allowedOrigins.js';
 
 const corsOptions = {
     origin: (origin, callback) => {
+        console.log(origin)
         if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
             callback(null, true);
         } else {
