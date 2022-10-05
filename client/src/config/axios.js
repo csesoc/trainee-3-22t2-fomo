10828764cfac946326ca49b6cfd8170ae9ba34c2
henryg <henryguo78@gmail.com>
2022-10-05 22:32:35 +1100

    Fixed issue with .env files again

--- client/src/config/axios.js
+++ client/src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import env from "react-dotenv";
 
-const BASE_URL = env.API_URL;
+const BASE_URL = process.env.REACT_APP_API_URL;
 
 export default axios.create({
   baseURL: BASE_URL,
