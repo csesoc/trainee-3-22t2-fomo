028cc8739eec9985448401017be6f42310c8fb05
henryg <henryguo78@gmail.com>
2022-10-05 20:44:49 +1100

    Changed FOMO_URL to frontend url and added API_URL for backend (in env file)

--- client/src/config/axios.js
+++ client/src/config/axios.js
@@ -1,14 +1,14 @@
-import axios from 'axios';
+import axios from "axios";
 import env from "react-dotenv";
 // Url of the server
-const BASE_URL = env.FOMO_URL;
+const BASE_URL = env.API_URL;
 
 export default axios.create({
-    baseURL: BASE_URL
+  baseURL: BASE_URL,
 });
 
 export const axiosPrivate = axios.create({
-    baseURL: BASE_URL,
-    headers: { 'Content-Type': 'application/json' },
-    withCredentials: true
+  baseURL: BASE_URL,
+  headers: { "Content-Type": "application/json" },
+  withCredentials: true,
 });
--- client/src/hooks/useRefreshToken.js
+++ client/src/hooks/useRefreshToken.js
@@ -1,30 +1,30 @@
 import useAuth from "./useAuth";
 import env from "react-dotenv";
 
-
 const useRefreshToken = () => {
-    const { setAuth } = useAuth();
+  const { setAuth } = useAuth();
 
-    const refresh = async () => {
-        let accessToken = undefined;
-        await fetch(env.FOMO_URL + '/refresh', {
-            method: "get",
-            headers: {
-              'Accept': 'application/json',
-              'Content-Type': 'application/json'
-            },
-            credentials: 'include'
-          })
-          .then((response) => response.json())
-          .then((data) => {
-            
-            setAuth({ accessToken: data.accessToken });
-            accessToken = data.accessToken;
-            })
-          .catch(err => {console.log(JSON.parse(err))});
-        return accessToken;
-    }
-    return refresh;
-}
+  const refresh = async () => {
+    let accessToken = undefined;
+    await fetch(env.API_URL + "/refresh", {
+      method: "get",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setAuth({ accessToken: data.accessToken });
+        accessToken = data.accessToken;
+      })
+      .catch((err) => {
+        console.log(JSON.parse(err));
+      });
+    return accessToken;
+  };
+  return refresh;
+};
 
 export default useRefreshToken;
